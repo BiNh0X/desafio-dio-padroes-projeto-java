91dbcdb26a0335995f8afededac1d83794e92942
BiNh0X <fabiocastro185@gmail.com>
2022-11-23 18:51:56 -0300

    Padrões de Projeto com Strategy.

--- dio-padroes-projeto-java/src/TestSingleton.java
+++ dio-padroes-projeto-java/src/TestSingleton.java
@@ -1,6 +1,11 @@
-import one.digitalinnovation.gof.SingletonEager;
-import one.digitalinnovation.gof.SingletonLazy;
-import one.digitalinnovation.gof.SingletonLazyHolder;
+import one.digitalinnovation.gof.Singleton.SingletonEager;
+import one.digitalinnovation.gof.Singleton.SingletonLazy;
+import one.digitalinnovation.gof.Singleton.SingletonLazyHolder;
+import one.digitalinnovation.gof.Strategy.Comportamento;
+import one.digitalinnovation.gof.Strategy.ComportamentoAgressivo;
+import one.digitalinnovation.gof.Strategy.ComportamentoDefensivo;
+import one.digitalinnovation.gof.Strategy.ComportamentoNormal;
+import one.digitalinnovation.gof.Strategy.Robo;
 
 public class TestSingleton {
     public static void main(String[] args) throws Exception {
@@ -24,5 +29,24 @@ public class TestSingleton {
 
         holder = SingletonLazyHolder.getInstancia();
         System.out.println(holder);
+
+        // Testes de Design Patterns 'Strategy':
+
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento agressivo = new ComportamentoAgressivo();
+    
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
+        robo.mover();
+
+        robo.setComportamento(defensivo);
+        robo.mover();
+        
+        robo.setComportamento(agressivo);
+        robo.mover();
+        robo.mover();
+        robo.mover();
     }
 }
